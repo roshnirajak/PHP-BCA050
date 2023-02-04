2a48a66f2f6b7c01ba91e3e614a28b81fb11184e
ROSH <imroshni3@gmail.com>
2023-02-04 20:38:08 +0530

    Create arithmetic_operations.php

---
+++ php-assignment/arithmetic_operations.php
@@ -0,0 +1,52 @@
+<?php
+
+if (isset($_POST['add'])) {
+    $varA = $_POST['varA'];
+    $varB = $_POST['varB'];
+    $sol = $varA + $varB;
+}
+if (isset($_POST['sub'])) {
+    $varA = $_POST['varA'];
+    $varB = $_POST['varB'];
+    $sol = $varA - $varB;
+}
+if (isset($_POST['divide'])) {
+    $varA = $_POST['varA'];
+    $varB = $_POST['varB'];
+    $sol = $varA / $varB;
+}
+if (isset($_POST['product'])) {
+    $varA = $_POST['varA'];
+    $varB = $_POST['varB'];
+    $sol = $varA * $varB;
+}
+?>
+<!DOCTYPE html>
+
+<head></head>
+
+<body>
+    <form method="post" onsubmit="retrun false">
+
+        Enter var a:
+        <input type="number" name="varA"><br>
+        Enter var b:
+        <input type="number" name="varB"><br>
+
+        <input type="submit" name="add" value="Add">
+        <input type="submit" name="sub" value="Subtract">
+        <input type="submit" name="divide" value="Divide">
+        <input type="submit" name="product" value="Multiply">
+
+        <span>
+            <?php
+            if (isset($sol)) {
+                echo ($sol);
+            }
+            ?>
+        </span>
+
+    </form>
+</body>
+
+</html>
