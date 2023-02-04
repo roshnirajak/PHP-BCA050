1c76f02fe5f65add86da1ab950e176d37ec2b797
ROSH <imroshni3@gmail.com>
2023-02-04 22:05:51 +0530

    Create 3)_verify.php

---
+++ php-assignment/3)_verify.php
@@ -0,0 +1,16 @@
+<?php
+    if(isset($_POST['submitBtn'])){
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $password = $_POST['pwd'];
+        $con_password = $_POST['con-pwd'];
+
+        if(($email=='test@gmail.com') && ($password==1234) && ($password==$con_password)){
+            echo ("Welcome " . $fname);
+        }
+        else{
+            header('location:3)_sign-up.php?q=0');
+        }
+    }
+?>
