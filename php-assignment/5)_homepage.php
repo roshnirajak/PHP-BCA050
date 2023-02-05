3d1f7f62b087ee395d410a00a0a9a832bca04db5
ROSH <imroshni3@gmail.com>
2023-02-05 22:57:09 +0530

    updated

--- php-assignment/5)_homepage.php
+++ php-assignment/5)_homepage.php
@@ -1,10 +1,27 @@
+<?php
+    if (isset($_COOKIE['user'])) {
+        $status = 1;
+    }
+    else{
+        header('location:5)_login.php');
+    }
+?>
 <!DOCTYPE html>
+
 <head></head>
+
 <body>
-    <h2>Welcome to Homepage</h2>
-    This is homepage of the orders website<br>
-    You can view order from other page<br>
-    by clicking on the button bellow...<br>
-    <a href="5)_orders.php"><button>Orders Page</button></a>
+    <?php
+    if($status == 1) {
+        ?>
+        <h2>Welcome to Homepage</h2>
+        This is homepage of the orders website<br>
+        You can view order from other page<br>
+        by clicking on the button bellow...<br>
+        <a href="5)_orders.php"><button>Orders Page</button></a>
+        <?php
+    }
+    ?>
 </body>
+
 </html>
--- php-assignment/5)_orders.php
+++ php-assignment/5)_orders.php
@@ -1,35 +1,55 @@
+<?php
+    if (isset($_COOKIE['user'])) {
+        $status = 1;
+    }
+    else{
+        header('location:5)_login.php');
+    }
+?>
 <!DOCTYPE html>
+
 <head>
     <style>
-        .orBox{
-            background-color:#ddd;
+        .orBox {
+            background-color: #ddd;
         }
     </style>
 </head>
+
 <body align=center>
-    <h2>Welcome to Orders Page</h2>
-    <table align=center>
-        <tr>
-            <td><div class="orBox">
-                    <h3>Order 1</h3>This is Order 1 of this site
-                </div>
-            </td>
-            <td><div class="orBox">
-                <h3>Order 2</h3>This is Order 2 of this site
-                </div>
-            </td>
-        </tr>
-        <tr>
-            <td><div class="orBox">
-                <h3>Order 3</h3>This is Order 3 of this site
-                </div>
-            </td>
-            <td><div class="orBox">
-                <h3>Order 4</h3>This is Order 4 of this site
-                </div>
-            </td>
-        </tr>
-    </table>
+    <?php
+    if($status == 1){
+        ?>
+        <h2>Welcome to Orders Page</h2>
+        <table align=center>
+            <tr>
+                <td>
+                    <div class="orBox">
+                        <h3>Order 1</h3>This is Order 1 of this site
+                    </div>
+                </td>
+                <td>
+                    <div class="orBox">
+                        <h3>Order 2</h3>This is Order 2 of this site
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <div class="orBox">
+                        <h3>Order 3</h3>This is Order 3 of this site
+                    </div>
+                </td>
+                <td>
+                    <div class="orBox">
+                        <h3>Order 4</h3>This is Order 4 of this site
+                    </div>
+                </td>
+            </tr>
+        </table>
+        <?php
+    }
+    ?>
 </body>
 
 </html>
